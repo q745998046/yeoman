f7696bc9999ac132e44b3456ccd5be35120e3e33
xiebin <745998046@qq.com>
2016-10-12 11:16:09 +0800

    11

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoApp', [
+    'ngSanitize'
+  ]);
